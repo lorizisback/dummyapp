5c2d58d3ec366c86d2e4e38501e5f9d342fdd73e
Marco Loriga <marco.loriga@imaginecurve.com>
2020-10-31 20:02:30 +0000

    Exercise one - Main Activity with layout

--- app/src/main/java/org/mastergluteus/testapp/MainActivity.kt
+++ app/src/main/java/org/mastergluteus/testapp/MainActivity.kt
@@ -1,23 +1,13 @@
 package org.mastergluteus.testapp
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.actvity_main.*
 
-class MainActivity : AppCompatActivity(R.layout.actvity_main) {
+class MainActivity : AppCompatActivity() {
 
-    override fun onStart() {
-        super.onStart()
-
-        button_continue.setOnClickListener {
-            navigateToDetail(text_input.text.toString())
-        }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.actvity_main)
     }
-
-    fun navigateToDetail(text: String) {
-        val intent = Intent(this, DetailActivity::class.java)
-        intent.putExtra(DetailActivity.DETAIL_EXTRA_KEY, text)
-        startActivity(intent)
-    }
+    
 }
